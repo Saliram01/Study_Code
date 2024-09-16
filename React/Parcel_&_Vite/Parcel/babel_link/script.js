43c0cff06cb96c463a7d05ce85617bcbde22c9ce
Saliram Chaudhary <saliramchaudhary1@gmail.com>
2024-09-16 16:42:00 +0530

    add ne files

---
+++ React/Parcel_&_Vite/Parcel/babel_link/script.js
@@ -0,0 +1,12 @@
+// const h2 = <h2 className="heading" id="heading">Using Babel Command for JSX</h2>
+// console.log(h2);
+
+function App(){
+    return (
+        <div>
+            <h2>Hello Babel</h2>
+        </div>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>); 
