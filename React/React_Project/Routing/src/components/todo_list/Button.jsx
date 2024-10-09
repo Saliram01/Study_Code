0b646c588ee3d892461e874b7a6f07416e49023b
Saliram Chaudhary <saliramchaudhary1@gmail.com>
2024-10-09 18:48:25 +0530

    update

--- React/React_Project/Routing/src/components/todo_list/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Button() {
-  return (
-    <button style={{padding : '6px 0', width : '100%',cursor : 'pointer',border : '2px solid white', borderRadius : '50px',}}>Add Todo</button>
-  )
-}
-
-export default Button
--- React/React_Project/Rp__/src/components/pages/ScrollUp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ScrollUp() {
-  return (
-    <button className='w-12 h-12 fixed right-4 bottom-6 bg-neutral-400 hover:bg-neutral-500 rounded-full text-white scroll-smooth'><i className="fa-solid fa-arrow-up"></i></button>
-  )
-}
-
-export default ScrollUp
