5cb53ac313598c591796175063bb947e637a261e
Saliram Chaudhary <saliramchaudhary1@gmail.com>
2024-10-19 21:20:30 +0530

    delete folder

--- Tz/script.js
+++
@@ -1,8 +0,0 @@
-const container = React.createElement('div',{className : 'container'},[
-    React.createElement('h1',{key : '1'},'React Element',[
-
-    ]),
-    React.createElement('p',{key : '2'},'I am saliram chaudhary from india . i m pursuing Web Developer')
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(container);
